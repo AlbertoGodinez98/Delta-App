9a5c3142d47d58f67968b2ba1dec147134db0dcd
AlbertoGodinez98 <albertogodnez98@gmail.com>
2022-05-24 19:39:26 -0500

    Added __post_init__

--- Delta-App.py
+++ Delta-App.py
@@ -1,11 +1,9 @@
 from Lib import *
 
 
-# logging.info("info")
-
-x = Inv("DL-05", 20)
-# x.info()
-#print("Initial quantity", x.current_quantity)
-x.operation(15, "remove")
-x.operation(50, "+")
-#print("Current quantity", x.current_quantity)
+DL_05 = Inv("DL-05", 20)
+DL_06 = Inv("DL-06", 100)
+DL_05.operation(15, "remove")
+DL_05.operation(50, "+")
+DL_06.operation(30, "-")
+DL_06.show()
--- Lib.py
+++ Lib.py
@@ -1,8 +1,7 @@
-from cmath import log
 from dataclasses import dataclass, field
 from datetime import date
 import logging
-
+import PySimpleGUI as sg
 
 logging.basicConfig(level=logging.INFO, filename="logging.log",
                     filemode="w", format="%(asctime)s - %(message)s")
@@ -12,8 +11,11 @@ logging.basicConfig(level=logging.INFO, filename="logging.log",
 class Inv():
     """Inv(name: str, current_quantity: int, day = date.today())"""
     name: str
-    current_quantity: int = 0
-    day: str = date.today()
+    current_quantity: int = field(default=0)
+    day: str = field(default=date.today())
+
+    def __post_init__(self) -> None:
+        return f"Created \"{self.name}\" on {self.day}"
 
     def operation(self, quantity: int = 0, action: str = "+") -> None:
         """Inv.operation(quantity: int, action: str)
@@ -22,11 +24,11 @@ class Inv():
             case "add" | "+":
                 self.current_quantity += quantity
                 logging.info(
-                    f"Added   {quantity}. Current quantity is {self.current_quantity}")
+                    f"Added   {quantity} to   \"{self.name}\". Current quantity is {self.current_quantity}")
             case "remove" | "-":
                 self.current_quantity -= quantity
                 logging.info(
-                    f"Removed {quantity}. Current quantity is {self.current_quantity}")
+                    f"Removed {quantity} from \"{self.name}\". Current quantity is {self.current_quantity}")
             case _:
                 logging.raiseExceptions("Action can only be 'Add' or 'Remove'")
                 logging.error("Error - Invalid action")
@@ -37,4 +39,5 @@ class Inv():
         print(
             f"Created \"{self.name}\" on {self.day}. Current quantity is {self.current_quantity}")
 
-    logging.info(f"Created {self.name}")
+    def show(self) -> None:
+        sg.Window(title="Hello world", layout=[[]], margins=(100, 50)).read()
